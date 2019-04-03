fbbd3f74f736e0068d6a3406561e401aeaf47e69
Santiago Gabriel Vollmar <santiago.vollmar@nyp.ch>
2019-04-03 19:22:52 +0200

    WE DON'T NEED NO SECURITY!

--- src/main/java/ch/zhaw/vivi/config/security/SecurityController.java
+++ src/main/java/ch/zhaw/vivi/config/security/SecurityController.java
@@ -17,16 +17,14 @@ public class SecurityController {
 	@PostMapping({"", "/"})
 	public ResponseEntity<String> login(@RequestBody User user) {
 		// eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1IiwiZXhwIjoxNTU0MzEyNzMwfQ.H0fNEh7IlNAZWvRv2zy-2pYdyOPxRk9Afo6uvwLXex3KycCzMI3CUNCZb3OWbwWEbPEkYFOV5iZ37KHb_5ET-A
+		
 		HttpHeaders responseHeaders = new HttpHeaders();
-		URI location = null;
-		try {
-			location = new URI("http://localhost:8080/login");
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    responseHeaders.setLocation(location);
-	    responseHeaders.set("Authorization", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1IiwiZXhwIjoxNTU0MzEyNzMwfQ.H0fNEh7IlNAZWvRv2zy-2pYdyOPxRk9Afo6uvwLXex3KycCzMI3CUNCZb3OWbwWEbPEkYFOV5iZ37KHb_5ET-A");
-		return new ResponseEntity<>("login successful!", HttpStatus.OK);
+		responseHeaders.set("Authorization", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1IiwiZXhwIjoxNTU0MzEyNzMwfQ.H0fNEh7IlNAZWvRv2zy-2pYdyOPxRk9Afo6uvwLXex3KycCzMI3CUNCZb3OWbwWEbPEkYFOV5iZ37KHb_5ET-A");
+		responseHeaders.set("Baeldung-Example-Header",
+				"Value-ResponseEntityBuilderWithHttpHeaders");
+
+		return ResponseEntity.ok()
+				.headers(responseHeaders)
+				.body("Response with header using ResponseEntity");
 	} 
 }
